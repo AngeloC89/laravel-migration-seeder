e1dd5a9edc6917c9eb9504a946fd3632191173c9
AngeloC89 <Angelociulla89@gmail.com>
2024-05-23 15:32:19 +0200

    i make the TRainsTableSeeder and have seeding a row on database

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Train;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -14,6 +16,16 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $new_train = new Train();
+        $new_train->azienda = 'Trenitalia';
+        $new_train->stazione_di_partenza = 'Napoli';
+        $new_train->stazione_di_arrivo = 'Milano';
+        $new_train->ora_di_partenza = '08:30:00';
+        $new_train->ora_di_arrivo = '10:30:00';
+        $new_train->codice_treno = 'TRE123';
+        $new_train->numero_carrozze = 100;
+        $new_train->in_orario = true;
+        $new_train->cancellato = false;
+        $new_train->save();
     }
 }
